37ec40712daaf71aaa17450294f8066af68d2429
Ameer Shaik <ameersk1401@gmail.com>
2025-06-23 13:06:39 -0400

    Ansible Hands-ons

---
+++ ansible/project_structure.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Author: Ameer Shaik
+
+mkdir inventory
+mkdir inventory/host_vars
+mkdir inventory/group_vars
+
+mkdir roles
+mkdir roles/python
+mkdir roles/python/tasks
+mkdir roles/python/handlers
